5109c0f251591f1d8d1c05cb5080b57756fed156
David <damusa2003@gmail.com>
2026-04-23 17:52:03 +0200

    Fixing some bugs and adding new functionalities

--- src/api/tagApi.ts
+++ src/api/tagApi.ts
@@ -13,6 +13,21 @@ type HalTag = Tag & {
     id?: number;
 };
 
+type HalContent = {
+    contentId?: number;
+    name?: string;
+    body?: string;
+    description?: string;
+    visibility?: string;
+    uri?: string;
+    _links?: {
+        self?: {
+            href?: string;
+        };
+        [key: string]: unknown;
+    };
+};
+
 export class TagsService {
     constructor(private authProvider: AuthProvider) {}
 
@@ -54,8 +69,60 @@ export class TagsService {
         return mergeHal<Tag>(resource);
     }
 
-    async findById(id: number | string): Promise<Tag> {
+    async findById(id: number | string): Promise<HalTag> {
         const resource = await getHal(`/tags/${id}`, this.authProvider);
-        return mergeHal<Tag>(resource);
+        const tag = mergeHal<Tag>(resource) as HalTag;
+
+        const href =
+            (resource as any)?._links?.self?.href ??
+            tag?._links?.self?.href ??
+            (resource as any)?.link?.("self")?.href;
+
+        const uri =
+            (resource as any)?.uri ??
+            tag?.uri ??
+            (href ? new URL(href).pathname : undefined);
+
+        const idFromUri = uri?.split("/").pop();
+
+        return {
+            ...tag,
+            uri,
+            _links: (resource as any)?._links ?? tag?._links,
+            id: tag.id ?? (idFromUri ? Number(idFromUri) : undefined),
+        };
+    }
+
+    async findContentsByTagId(tagId: number): Promise<HalContent[]> {
+        const resource = await getHal(
+            `/contents/search/findByTags_Id?tagId=${tagId}`,
+            this.authProvider
+        );
+
+        const embedded = resource.embeddedArray("contents") || [];
+
+        return embedded.map((item: any) => {
+            const content = mergeHal<any>(item) as HalContent;
+
+            const href =
+                item?._links?.self?.href ??
+                content?._links?.self?.href ??
+                item?.link?.("self")?.href;
+
+            const uri =
+                item?.uri ??
+                content?.uri ??
+                (href ? new URL(href).pathname : undefined);
+
+            const idFromUri = uri?.split("/").pop();
+
+            return {
+                ...content,
+                uri,
+                _links: item?._links ?? content?._links,
+                contentId:
+                    content.contentId ?? (idFromUri ? Number(idFromUri) : undefined),
+            };
+        });
     }
 }
