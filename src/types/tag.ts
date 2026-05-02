1d56a60d15fecae75d3b3b56df1249c3ae6af9c4
arp137 <154314080+arp137@users.noreply.github.com>
2026-05-02 20:03:39 +0200

    Implementation of Tag type

---
+++ src/types/tag.ts
@@ -0,0 +1,12 @@
+﻿import { Resource } from "halfred";
+
+export interface TagEntity {
+    id?: number;
+    uri?: string;
+    name: string;
+    description?: string;
+    created?: Date;
+    modified?: Date;
+}
+
+export type Tag = TagEntity & Resource;
